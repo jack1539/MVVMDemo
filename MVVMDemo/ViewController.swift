da0b7927dc6442d75a2a9ccb10ca5887a8352ccb
Huang, Jinsheng X. -ND <Jinsheng.X.Huang.-ND@disney.com>
2017-08-21 11:19:14 +0800

    Init project.

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Huang, Jinsheng X. -ND on 21/08/2017.
+//  Copyright © 2017 hjs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
